e922200cd7ee345875974d550c19e3f7bc581fc8
Isaac Basque-Rice <ibasquerice@gmail.com>
2022-04-20 17:38:26 +0100

    began work on currency converter program

--- 1-Beginner/2-CurrencyConverter/Rust/CurrencyConverter/src/main.rs
+++ 1-Beginner/2-CurrencyConverter/Rust/CurrencyConverter/src/main.rs
@@ -1,5 +1,27 @@
 // Develop a program to convert currency X to currency Y and visa versa.
 
+use std::io;
+
+fn input_to_int(&mut input: String) {
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read from stdin");
+
+    let mut num = input.trim().parse::<i32>().unwrap();
+
+    return num;
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Currency conversion program!")
+
+    let mut num_to_input = String::new();
+    let mut num_from_input = String::new();
+
+    let mut num_to = input_to_int(num_to_input);
+    let mut num_from = input_to_int(num_from_input);
+
+    println!("Converting £{} to ${}", num_to, num_from)
 }
---
+++ 1-Beginner/2-CurrencyConverter/Rust/CurrencyConverter/target/debug/deps/CurrencyConverter.d
@@ -0,0 +1,5 @@
+D:\Misc\Non-Uni\Projects\language-learning-projects\1-Beginner\2-CurrencyConverter\Rust\CurrencyConverter\target\debug\deps\CurrencyConverter.exe: src\main.rs
+
+D:\Misc\Non-Uni\Projects\language-learning-projects\1-Beginner\2-CurrencyConverter\Rust\CurrencyConverter\target\debug\deps\CurrencyConverter.d: src\main.rs
+
+src\main.rs:
